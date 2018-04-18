16e33347ecc6fdc2791c36b1a7a2569d8dd7f7de
Torne (Richard Coles) <torne@google.com>
2018-04-18 18:05:34 -0400

    send errors about .current_* to /dev/null

--- home/.bashrc.d/45-screen.sh
+++ home/.bashrc.d/45-screen.sh
@@ -1,5 +1,5 @@
 # restore DISPLAY and SSH_AUTH_SOCK from those saved before entering screen
 if [ -n "$STY" ]; then
-  export DISPLAY=`cat ~/.current_display`
-  export SSH_AUTH_SOCK=`cat ~/.current_ssh_auth_sock`
+  export DISPLAY=`cat ~/.current_display 2>/dev/null`
+  export SSH_AUTH_SOCK=`cat ~/.current_ssh_auth_sock 2>/dev/null`
 fi
