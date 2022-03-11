ea2e0f6b2d6baa1e25c3427b6dcfdeb7a8fc4523
Torne Wuff <torne@wolfpuppy.org.uk>
2022-03-11 14:01:38 -0500

    remove nvm

--- home/.bashrc.d/51-nvm.sh
+++
@@ -1,4 +0,0 @@
-# Load nvm to get node support.
-export NVM_DIR=$(realpath ~/.nvm)
-[ -r "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-[ -r "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
